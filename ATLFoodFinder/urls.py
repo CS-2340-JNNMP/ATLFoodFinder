7080eafd2df75f5976ee7827b8e5be0210ba7cbb
JevonTwitty <twittyjevon@gmail.com>
2024-09-14 05:33:42 -0400

    Created Front and map page. Created the beginnings of the base template we'll be using for every head, which is essentially just the head navigation bar.

--- ATLFoodFinder/urls.py
+++ ATLFoodFinder/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import index, map
+
 urlpatterns = [
+    path('', index, name='index'),
+    path('map/',map, name='map'),
     path('admin/', admin.site.urls),
 ]
