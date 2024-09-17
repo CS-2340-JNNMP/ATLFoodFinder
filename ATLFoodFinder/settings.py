ddae61c3314655f77392fed0a125054b302bd49f
NityaPotti <nityapotti01@gmail.com>
2024-09-17 16:34:54 -0400

    h

--- ATLFoodFinder/settings.py
+++ ATLFoodFinder/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ATLFoodFinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ACCOUNTS_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
