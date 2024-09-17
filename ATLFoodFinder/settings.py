dbdad1d57b8cc08452f98b49bd08eef35920f3be
NityaPotti <nityapotti01@gmail.com>
2024-09-17 16:41:54 -0400

    Revert "h"
    This reverts commit ddae61c3314655f77392fed0a125054b302bd49f.

--- ATLFoodFinder/settings.py
+++ ATLFoodFinder/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ATLFoodFinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ACCOUNTS_DIR / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
