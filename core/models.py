0278e6dd5a4b856ca8566faa815e8ff0f81a1df8
NityaPotti <nityapotti01@gmail.com>
2024-09-25 16:13:19 -0400

    hi

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     address = models.TextField()
     rating = models.FloatField(null=True, blank=True)
     place_id = models.CharField(max_length=255, unique=True)
