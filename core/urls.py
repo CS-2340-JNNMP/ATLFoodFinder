7d939903e2a8b7a5d2dbc806444f19dd87efb678
NityaPotti <nityapotti01@gmail.com>
2024-10-01 16:43:13 -0400

    Remove Saved Function

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_user, name='logout'),
 path('save_restaurant/<str:place_id>/', views.save_restaurant, name='save_restaurant'),
     path('saved_restaurants/', views.saved_restaurants, name='saved_restaurants'),
+    path('unsave_restaurant/<str:place_id>/', views.unsave_restaurant, name='unsave_restaurant'),
 
 ]
